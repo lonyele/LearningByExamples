7bc75d8e3b7604e9a68f9abdce75cd7aee0932eb
Taeheon Kim <skyrabbits1@gmail.com>
2018-11-07 18:23:14 +0900

    MST의 model 하고 typescript 중복이 너무 많아서  MST껄로 써봄. 좀 더 다듬어 봄

--- packages/learn-mst/src/components/molecules/MemoItem/MemoItem.tsx
+++ packages/learn-mst/src/components/molecules/MemoItem/MemoItem.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { IMemoItem } from "../../../models/memoItem/memoItem";
+import { IMemoItem } from "./models/MemoItem.model";
 import { MemoItem } from "./styles/MemoItem.style";
 
 export default (props: IMemoItem) => {
--- packages/learn-mst/src/models/root/root.ts
+++ packages/learn-mst/src/models/root/root.ts
@@ -1,12 +1,12 @@
 import { types } from "mobx-state-tree";
-import { MemoItem } from "../memoItem";
+import { MemoItemModel } from "../../components/molecules/MemoItem";
 
 export const RootStore = types
   .model({
-    memo: types.array(MemoItem)
+    memo: types.array(MemoItemModel)
   })
   .actions(self => ({
     addMemoItem(content: string = "whattt", completed: boolean = false) {
-      self.memo.push(MemoItem.create({ content, completed }));
+      self.memo.push(MemoItemModel.create({ content, completed }));
     }
   }));
