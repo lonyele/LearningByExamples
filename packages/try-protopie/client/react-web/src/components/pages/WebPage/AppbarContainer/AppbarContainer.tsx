040708e9149a6768efaf6717cda29d75aa417890
Taeheon Kim <skyrabbits1@gmail.com>
2018-11-12 20:04:41 +0900

    Video 버튼

--- packages/try-protopie/client/react-web/src/components/pages/WebPage/AppbarContainer/AppbarContainer.tsx
+++ packages/try-protopie/client/react-web/src/components/pages/WebPage/AppbarContainer/AppbarContainer.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 
 import { ImageButton } from "./ImageButton";
+import { VideoButton } from "./VideoButton";
 export class AppbarContainer extends React.Component<any, any> {
   public render() {
     return (
-      <div style={{ border: "1px solid black", height: "50px" }}>
+      <div
+        style={{ border: "1px solid black", height: "50px", display: "flex" }}
+      >
         <ImageButton />
+        <VideoButton />
       </div>
     );
   }
